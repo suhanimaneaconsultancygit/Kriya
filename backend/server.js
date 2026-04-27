d8908337d487cf7753525429ca530f5ff9c6b19f
Suhanimane04 <160301814+Suhanimane04@users.noreply.github.com>
2026-04-27 14:20:22 +0530

    Url added backend

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 
 // ─── MIDDLEWARE ───────────────────────────────────────────────────────────────
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:5173"],
+  origin: ["http://localhost:3000", "http://localhost:5173","kriya-pi.vercel.app"],
   credentials: true,
 }));
 app.use(express.json({ limit: "50mb" }));
